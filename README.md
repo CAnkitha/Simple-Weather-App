3f5c05b8fe53edb72d1b1bd102c51a4fb53bc2e3
AnkithaCheruku <AnkithaCheruku@example.com>
2024-04-23 16:18:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Weather App
+This is a simple weather application built with HTML, CSS, and JavaScript. It allows users to search for a city and view its current weather information.
+
+Features
+Search: Users can input the name of a city to search for its weather information.
+Display: The app displays the current temperature, weather condition, and location.
+Background: The background changes dynamically based on the weather conditions.
+Getting Started
+To run this weather app locally, follow these steps:
+
+Clone this repository to your local machine:
+bash
+Copy code
+git clone <repository-url>
+Open the index.html file in your web browser.
+Usage
+Enter the name of a city in the search box and press Enter.
+The app will fetch the current weather data for the entered city and display it.
+Dependencies
+This project uses the OpenWeatherMap API to fetch weather data. Make sure you have an API key from OpenWeatherMap and replace '0ac3a0ed570e0a6cbf40cc15d36cf703' in main.js with your API key.
+
+License
+This project is licensed under the MIT License.
---
+++ main.js
@@ -0,0 +1,49 @@
+const api = {
+  key: "0ac3a0ed570e0a6cbf40cc15d36cf703",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+ 
+}
+
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
